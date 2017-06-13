d4406b924f8fa1e45d36379a4fbe0f9e61b446ea
Agnieszka Wakula <agnieszka.wakula@gmail.com>
2017-06-13 23:12:59 +0200

    correction instruction if

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,14 @@ var b = 3;
 var exValue = (a * a) + (2 * a * b) - (b * b);
 
 console.log('ex field with base a: ' + a + ' and b: ' + b + ' is equal to: ' + exValue);
-var wynik = prompt(exValue);
+
 alert('Wynik to: ' + exValue);
 
-var wynik = 1;
-if ( wynik >= 1 ) {
+
+if (exValue > 0) {
     console.log('Wynik dodatni');
-} else if ( wynik < 0 ) {
+} else if (exValue < 0) {
     console.log('Wynik ujemny');
-} else if (wynik == 0) { 
+} else {
     console.log('Wynik to 0');
-} else { 
-console.log('Your result');
 }
